afc3ca3bc552f591c1129a04298b4390baa189b4
WXXUPT <1454291663@qq.com>
2020-11-29 18:23:04 +0800

    4

--- leetCode刷题记录/main.m
+++ leetCode刷题记录/main.m
@@ -206,3 +206,24 @@ int calculate(char* s){
     }
     return x + y;
 }
+/*
+ 剑指 Offer 42. 连续子数组的最大和
+ 
+ 输入一个整型数组，数组中的一个或连续多个整数组成一个子数组。求所有子数组的和的最大值。
+
+ 要求时间复杂度为O(n)。
+ */
+int maxSubArray(int* nums, int numsSize){
+    int answer = nums[0], number = nums[0];
+    for (int i = 1; i < numsSize; i++) {
+        if (number + nums[i] < nums[i]) {
+            number = nums[i];
+        } else {
+            number = nums[i] + number;
+        }
+        if (answer < number) {
+            answer = number;
+        }
+    }
+    return answer;
+}
