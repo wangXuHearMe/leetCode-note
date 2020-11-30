2df0560ceaff8dcd8f122bca45d2c1d416b04200
WXXUPT <1454291663@qq.com>
2020-11-30 17:58:52 +0800

    5

--- leetCode刷题记录/main.m
+++ leetCode刷题记录/main.m
@@ -227,3 +227,43 @@ int maxSubArray(int* nums, int numsSize){
     }
     return answer;
 }
+/*
+ 27. 移除元素
+ 给你一个数组 nums 和一个值 val，你需要 原地 移除所有数值等于 val 的元素，并返回移除后数组的新长度。
+
+ 不要使用额外的数组空间，你必须仅使用 O(1) 额外空间并 原地 修改输入数组。
+
+ 元素的顺序可以改变。你不需要考虑数组中超出新长度后面的元素
+ */
+int removeElement(int* nums, int numsSize, int val){
+    for (int i = 0; i < numsSize - 1; i++) {
+        for (int j = 0; j < numsSize - i - 1; j++) {
+            if (nums[j] > nums[j + 1]) {
+                int temp = nums[j + 1];
+                nums[j + 1] = nums[j];
+                nums[j] = temp;
+            }
+        }
+    }
+    int count = 0, intex = 0;
+    for (int i = 0; i < numsSize; i++) {
+        if (nums[i] == val) {
+            intex = i;
+            break;
+        }
+    }
+    for (int i = 0; i < numsSize; i++) {
+        if (nums[i] == val) {
+            count++;
+        }
+    }
+    int numerCount = 1;
+    for (int i = numsSize - 1; numerCount <= count; i--) {
+            int temp = nums[i];
+            nums[i] = nums[intex];
+            nums[intex] = temp;
+            intex++;
+            numerCount++;
+    }
+    return numsSize - count;
+}
