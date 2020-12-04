b65cfc8752743119197e11deea48a68919309ebf
WXXUPT <1454291663@qq.com>
2020-12-04 17:08:21 +0800

    1

--- leetCode刷题记录/main.m
+++ leetCode刷题记录/main.m
@@ -532,3 +532,22 @@ int sumNums(int n) {
     n && (n += sumNums(n - 1));
     return n;
 }
+/*
+ 剑指 Offer 03. 数组中重复的数字
+
+ 找出数组中重复的数字。
+
+
+ 在一个长度为 n 的数组 nums 里的所有数字都在 0～n-1 的范围内。数组中某些数字是重复的，但不知道有几个数字重复了，也不知道每个数字重复了几次。请找出数组中任意一个重复的数字
+ */
+int findRepeatNumber(int* nums, int numsSize){
+    int hash[100000] = {0}, i = 0;
+    for (; i < numsSize; i++) {
+        if (0 == hash[nums[i]]) {
+            hash[nums[i]] = 1;
+        } else {
+            return nums[i];
+        }
+    }
+    return -1;
+}
