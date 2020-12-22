51442e00a3f9ccd9daa05c2c8f13fa05363ab979
WXXUPT <1454291663@qq.com>
2020-12-22 15:35:31 +0800

    12.22

--- leetCode刷题记录/main.m
+++ leetCode刷题记录/main.m
@@ -623,3 +623,21 @@ int* getLeastNumbers(int* arr, int arrSize, int k, int* returnSize){
     *returnSize = k;
     return answer;
 }
+/*
+ 136. 只出现一次的数字
+ 给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。
+
+ 说明：
+
+ 你的算法应该具有线性时间复杂度。 你可以不使用额外空间来实现吗？
+ */
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+        nums.sort()
+        for i in range(1, len(nums), 2):
+            if nums[i-1] != nums[i]:
+                return nums[i-1]
+            if (i+2) == len(nums):
+                return nums[-1]
